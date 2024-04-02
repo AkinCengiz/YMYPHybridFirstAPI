11b3e3c3e7b0f3c985ed23df630968527bc8ecfb
Akın Cengiz <akincengiz@outlook.com.tr>
2024-04-02 22:20:43 +0300

    Add DTO models

---
+++ YMYPHybridFirst/Dto/GetAllProductsResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace YMYPHybridFirst.Dto;
+
+public class GetAllProductsResponseDTO
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int UnitsInStock { get; set; }
+}
